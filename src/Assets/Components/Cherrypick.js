c25306401d4ea17538a65a8dd9e7036de516d774
Hussainshah821 <ssyedhussain821@gmail.com>
2024-02-03 10:59:50 +0500

    Commit #100

---
+++ src/Assets/Components/Cherrypick.js
@@ -0,0 +1,12 @@
+import React from "react";
+function Cherrypick(){
+    return(
+        <>
+        <h1>Cherry Picking</h1>
+        <p>The cherry picking feature allows you to select specific commits in a branch</p>
+        <p>This commit is commit#100 </p>
+        </>
+    );
+
+};
+export default Cherrypick;
