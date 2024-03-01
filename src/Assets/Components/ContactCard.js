871202397c4f22b5c0f0b63d4d6d901cb3eb6114
Hussainshah821 <ssyedhussain821@gmail.com>
2024-03-01 12:36:51 +0500

    Re write the code for cards

--- src/Assets/Components/ContactCard.js
+++ src/Assets/Components/ContactCard.js
@@ -1,39 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FormLabel } from 'react-bootstrap';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
+
 function Contact() {
+  
+
   return (
     <>
-   
-    
-
-  
-    <Form>
-      <Row className='w-100 ps-4 pt-2'>
-      <Form.Control className= "inputlabels" placeholder="Nom complet" />
-      </Row>
-      <Row className='mt-3 w-100 mx-auto mb-2'>
-        <Col>
-          <Form.Control className= "inputlabels" placeholder="Téléphone *" />
-        </Col>
-        <Col>
-          
-          <Form.Control className= "inputlabels" type="email" placeholder="E-mail *" />
-        </Col>
-      </Row>
-      <Row className='w-100 ps-4 mb-2 pt-2'>
-        <FormLabel className='text-white text-start'>Message</FormLabel>
-        <Form.Control as="textarea" className='textarea' rows={3} placeholder="Write your message here..."/>
-     
-      </Row>
-    </Form>
- 
-
-
-
-</>
+      <Form>
+        <Row className='w-100 ps-4 pt-2'>
+          <Form.Control 
+            id="name" 
+            className="inputlabels" 
+            placeholder="Nom complet" 
+           
+          />
+        </Row>
+        <Row className='mt-3 w-100 mx-auto mb-2'>
+          <Col>
+            <Form.Control className="inputlabels" placeholder="Téléphone *" />
+          </Col>
+          <Col>
+            <Form.Control id="email" className="inputlabels" type="email" placeholder="E-mail *"  />
+                      </Col>
+        </Row>
+        <Row className='w-100 ps-4 mb-2 pt-2'>
+          <FormLabel className='text-white text-start'>Message</FormLabel>
+          <Form.Control as="textarea" className='textarea' rows={3} placeholder="Write your message here..." />
+        </Row>
+      </Form>
+    </>
   );
 }
 
--- src/Assets/Components/HomeCard.js
+++ src/Assets/Components/HomeCard.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { Row } from 'react-bootstrap';
 
-function HomeCard(){
+function HomeCard() {
 
-    return(
-<>
-          <Card.Text className='text-center homecontent pt-4 text-white'>
-            Léger et rapide à charger le Dolce Vita est notre modèle milieu de gamme qui vous 
-            sera utile pour tous types d’usage personnel comme professionnel. <br/><br/>Fini le stress 
+  return (
+    <>
+      <Card className='transparent-bg w-25'>
+        <Card.Body className='homecard p-2'>
+          <Card.Title><h1 className='text-white heading text-center title'>Dolce Vita</h1></Card.Title>
+
+          <Card.Text className='text-center homecontent pt-4 px-4 text-white'>
+            Léger et rapide à charger le Dolce Vita est notre modèle milieu de gamme qui vous
+            sera utile pour tous types d’usage personnel comme professionnel. <br /><br />Fini le stress
             des transports ou les pleins d’essence, avec votre Dolce Vita circuler librement et sans contrainte !
           </Card.Text>
+          <Row>
+            <Button variant="primary" className='card-button w-50 mx-auto'>          Réserver un essai
+            </Button>
+          </Row>
+          <Card.Text className='text-white text-center'>
+            Voir les caractéristiques
+          </Card.Text>
+        </Card.Body>
+      </Card>
 
-          </>
-          );
+    </>
+  );
 };
 export default HomeCard;
